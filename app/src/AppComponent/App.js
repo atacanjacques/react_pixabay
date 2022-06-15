5aec0a6368e2c27b7d60bb87931a6ed0b1af817b
atacanjacques <atacan.jacques@gmail.com>
2022-06-15 23:34:37 +0200

    Add input for search

--- app/src/AppComponent/App.js
+++ app/src/AppComponent/App.js
@@ -1,23 +1,29 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import pixabayApiService from '../services/pixabayApiService';
+import AppPicture from './AppPictureComponent/AppPicture';
 
 
 function App() {
-  const [searchImagesResult, setSearchImagesResult] = useState(0);
+  const [searchImagesResult, setSearchImagesResult] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("yellow flowers");
 
   useEffect(() => {
-    pixabayApiService.searchImagesByQuery("yellow flowers")
+    // Retrieve images from Pixabay 
+    pixabayApiService.searchImagesByQuery(searchQuery)
       .then((result) => {
         return setSearchImagesResult(result.hits);
-      })
-  }, []);
+      });
+  }, [searchQuery]);
 
 
   return (
     <div className="App">
+      <input type="text" placeholder="Search ..." onChange={(e) => setSearchQuery(e.target.value)}/>
+
         {Object.keys(searchImagesResult).map((key) => {
-          return <img src={searchImagesResult[key].previewURL} />
+          let item = searchImagesResult[key];
+          return <AppPicture key={key} data={item} />
         })}
     </div>
   );
