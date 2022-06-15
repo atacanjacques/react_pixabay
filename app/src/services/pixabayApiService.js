954d11f34e9ea27a99ff8607fbb9f3a93ded96ab
atacanjacques <atacan.jacques@gmail.com>
2022-06-15 22:57:11 +0200

    Add service to retrieve data from pixabay api

---
+++ app/src/services/pixabayApiService.js
@@ -0,0 +1,11 @@
+const apiBaseUrl = "https://pixabay.com/api/"
+const apiKey = process.env.REACT_APP_PIXABAY_API_KEY;
+
+exports.searchImagesByQuery = async (queryString) => {
+    queryString = queryString.replace(' ', "+")
+    
+    // Query Url : https://pixabay.com/api/?key=APIKEY&q=QUERY&image_type=photo&pretty=true
+    let queryUrl = `${apiBaseUrl}?key=${apiKey}&q=${queryString}&image_type=photo&pretty=true`
+
+    return await (await fetch(queryUrl)).json();
+}
