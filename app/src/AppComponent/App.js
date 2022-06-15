3c3d76b2b9b9ab50ef0c863b253945c59db75461
atacanjacques <atacan.jacques@gmail.com>
2022-06-15 22:57:28 +0200

    Test pixabay api service on a component

--- app/src/AppComponent/App.js
+++ app/src/AppComponent/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import pixabayApiService from '../services/pixabayApiService';
+
 
 function App() {
+  const [searchImagesResult, setSearchImagesResult] = useState(0);
+
+  useEffect(() => {
+    pixabayApiService.searchImagesByQuery("yellow flowers")
+      .then((result) => {
+        return setSearchImagesResult(result.hits);
+      })
+  }, []);
+
+
   return (
     <div className="App">
-      App
+        {Object.keys(searchImagesResult).map((key) => {
+          return <img src={searchImagesResult[key].previewURL} />
+        })}
     </div>
   );
 }
