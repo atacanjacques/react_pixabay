641d96e1757aebdc6c91dc2ffa5fe43e07171d65
atacanjacques <atacan.jacques@gmail.com>
2022-06-16 00:31:27 +0200

    Add basic style with tailwind

--- app/src/AppComponent/App.js
+++ app/src/AppComponent/App.js
@@ -18,13 +18,17 @@ function App() {
 
 
   return (
-    <div className="App">
-      <input type="text" placeholder="Search ..." onChange={(e) => setSearchQuery(e.target.value)}/>
+    <div className="m-10 p-5 rounded-lg border-solid border-2 border-gray-500 bg-gray-300">
+      <div className="w-full flex mb-5">
+        <input className="w-full md:w-auto p-1 pl-5 m-auto	border-solid border-1 border-gray-500 focus:outline-none placeholder:text-black" type="text" placeholder="Search ..." onChange={(e) => setSearchQuery(e.target.value)} />
+      </div>
 
+      <div className="grid md:grid-cols-3 gap-4">
         {Object.keys(searchImagesResult).map((key) => {
           let item = searchImagesResult[key];
           return <AppPicture key={key} data={item} />
         })}
+      </div>
     </div>
   );
 }
