601aee6487fdb8da5e1cf438a867e44eb5a70e22
atacanjacques <atacan.jacques@gmail.com>
2022-06-16 12:07:17 +0200

    Add errror handling for api service

--- app/src/services/pixabayApiService.js
+++ app/src/services/pixabayApiService.js
@@ -2,10 +2,15 @@ const apiBaseUrl = "https://pixabay.com/api/"
 const apiKey = process.env.REACT_APP_PIXABAY_API_KEY;
 
 exports.searchImagesByQuery = async (queryString) => {
-    queryString = queryString.replace(' ', "+")
+    queryString = queryString.replace(' ', "+");
     
     // Query Url : https://pixabay.com/api/?key=APIKEY&q=QUERY&image_type=photo&pretty=true
-    let queryUrl = `${apiBaseUrl}?key=${apiKey}&q=${queryString}&image_type=photo&pretty=true`
+    let queryUrl = `${apiBaseUrl}?key=${apiKey}&q=${queryString}&image_type=photo&pretty=true`;
 
-    return await (await fetch(queryUrl)).json();
+    try {
+        return await (await fetch(queryUrl)).json();
+    } catch (error) {
+        return false;        
+    }
+
 }
